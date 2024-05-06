dd85cdf52de77d6277106a2f78c8b8361c4417f4
Cleonaldo Andrade Freire <cleonaldo@users.noreply.github.com>
2024-05-06 00:20:14 +0000

    várias mudanças

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,36 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são acima da média" });
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  console.log(databaseVersionResult);
+  console.log(databaseVersionValue);
+
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "select count(*)::int from pg_stat_activity where datname = 'local_db';",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -2,3 +2,28 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+test("GET current datetime in field update_at ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const parseUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdatedAt);
+});
+
+test("GET database version in field dependencies.database.version", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+});
+
+test("GET database max_connections in field dependencies.database.max_connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+});
+
+test("GET database count opened connections in field dependencies.database.opened_connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
